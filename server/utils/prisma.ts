82427e0d08076d4ca443a1e965fd905b3909e497
Paul Barbenheim <paul@barbenheim.de>
2026-04-19 23:21:02 +0200

    update prisma to newest version and make it compatible with docker env variables

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -2,13 +2,35 @@ import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../../generated/prisma/client'
 
-const connectionString = process.env.DATABASE_URL
+const database = process.env.DATABASE_NAME;
+const host = process.env.DATABASE_HOST;
+const port = process.env.DATABASE_PORT;
+const user = process.env.DATABASE_USER;
+const password = process.env.DATABASE_PASSWORD;
 
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set')
+if (!database) {
+  throw new Error('DATABASE_NAME is not set')
+}
+if (!host) {
+  throw new Error('DATABASE_HOST is not set')
+}
+if (!port) {
+  throw new Error('DATABASE_PORT is not set')
+}
+if (!user) {
+  throw new Error('DATABASE_USER is not set')
+}
+if (!password) {
+  throw new Error('DATABASE_PASSWORD is not set')
 }
 
-const adapter = new PrismaPg({ connectionString })
+const adapter = new PrismaPg({
+  database,
+  host,
+  port: parseInt(port, 10),
+  user,
+  password,
+})
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
