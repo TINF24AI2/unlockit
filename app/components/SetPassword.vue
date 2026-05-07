cf5c4268c036f7b86707b316da9b897a9f842e02
cyTaurus <186215285+cyTaurus@users.noreply.github.com>
2026-05-07 18:25:48 +0200

    add reroute on first login & adjust password requirements

--- app/components/SetPassword.vue
+++ app/components/SetPassword.vue
@@ -36,8 +36,11 @@ const schema = z.object({
   oldPassword: z.string('Altes Passwort ist erforderlich').min(1, 'Altes Passwort ist erforderlich'),
 
   newPassword: z.string('Passwort ist erforderlich')
-    .min(12, 'Passwort muss mindestens 12 Zeichen lang sein')
-    .regex(/^[A-Za-z0-9!#$%&]+$/, 'Schwaches Passwort'),
+    .min(12, 'Passwort muss mindestens 12 Zeichen lang sein.')
+    .regex(/[a-z]/, 'Mindestens ein Kleinbuchstabe erforderlich.')
+    .regex(/[A-Z]/, 'Mindestens ein Großbuchstabe erforderlich.')
+    .regex(/\d/, 'Mindestens eine Zahl erforderlich.')
+    .regex(/[!#$%&]/, 'Mindestens ein Sonderzeichen erforderlich. \nErlaubte Sonderzeichen: !#$%&'),
 
   confirmPassword: z.string('Passwort wiederholen').min(1, 'Passwort wiederholen')
 })
@@ -53,22 +56,36 @@ const emit = defineEmits(['submit'])
 function onSubmit(payload: FormSubmitEvent<Schema>) {
   emit('submit', payload.data)
 }
+
+const route = useRoute()
+
+const isFirstLogin = computed(() => route.query.from === 'firstlogin')
 </script>
 
 <template>
-  <div class="flex justify-center">
-    <UPageCard class="w-full max-w-md bg-gray-400 border-3 border-green-400">
-      <UAuthForm
-        :schema="schema"
-        title="Passwort wählen"
-        :fields="fields"
-        :submit="{
-          label: 'Passwort zurücksetzen',
-          color: 'neutral'
-        }"
-        class="max-w-md"
-        @submit="onSubmit"
+  <div class="flex justify-center pt-1">
+    <div class="w-full max-w-md">
+      <UAlert
+        v-if="isFirstLogin"
+        title="Neues Passwort erforderlich"
+        description="Sie melden sich zum ersten Mal an. Bitte wählen Sie ein neues Passwort."
+        color="warning"
+        class="mb-4"
       />
-    </UPageCard>
+
+      <UPageCard class="w-full max-w-md bg-gray-400 border-3 border-green-400">
+        <UAuthForm
+          :schema="schema"
+          title="Passwort wählen"
+          :fields="fields"
+          :submit="{
+            label: 'Passwort zurücksetzen',
+            color: 'neutral'
+          }"
+          class="max-w-md"
+          @submit="onSubmit"
+        />
+      </UPageCard>
+    </div>
   </div>
 </template>
